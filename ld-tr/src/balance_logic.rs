7b20af359faaa455524cb3f88128a228e040fb0c
lopes <alexrozaestudos@gmail.com>
2025-07-11 19:34:09 -0300

    funcionou!

--- ld-tr/src/balance_logic.rs
+++ ld-tr/src/balance_logic.rs
@@ -1,9 +1,42 @@
-use std::env;
+use crate::check_health::HEALTH_CHECK;
+use actix_web::{HttpRequest, web};
 use once_cell::sync::Lazy;
+use reqwest::Client;
+use std::env;
 
-pub static PAYMENT_PROCESSOR_DEFAULT: Lazy<String> =
-    Lazy::new(|| env::var("PAYMENT_PROCESSOR_DEFAULT").expect("PAYMENT_PROCESSOR_DEFAULT url not set"));
+pub static PAYMENT_PROCESSOR_DEFAULT: Lazy<String> = Lazy::new(|| {
+    env::var("PAYMENT_PROCESSOR_DEFAULT").expect("PAYMENT_PROCESSOR_DEFAULT url not set")
+});
 
-pub static PAYMENT_PROCESSOR_FALLBACK: Lazy<String> =
-    Lazy::new(|| env::var("PAYMENT_PROCESSOR_FALLBACK").expect("PAYMENT_PROCESSOR_FALLBACK url not set"));
+pub static PAYMENT_PROCESSOR_FALLBACK: Lazy<String> = Lazy::new(|| {
+    env::var("PAYMENT_PROCESSOR_FALLBACK").expect("PAYMENT_PROCESSOR_FALLBACK url not set")
+});
 
+pub async fn call_payments(req: HttpRequest, body: web::Bytes) {
+    let client = Client::new();
+
+    let base_url = if HEALTH_CHECK.is_failed() {
+        PAYMENT_PROCESSOR_FALLBACK.as_str()
+    } else {
+        PAYMENT_PROCESSOR_DEFAULT.as_str()
+    };
+
+    let full_url = format!("{}{}", base_url, req.uri());
+
+    let method = req.method().to_string().parse::<reqwest::Method>().unwrap();
+    let mut request_builder = client.request(method, full_url);
+
+    for (key, value) in req.headers().iter() {
+        if let Ok(val_str) = value.to_str() {
+            request_builder = request_builder.header(key.as_str(), val_str);
+        }
+    }
+
+    match request_builder.body(body.clone()).send().await {
+        Ok(res) => {println!("{:?}",res)}
+        Err(_) => {
+            println!("error ;-;");
+            HEALTH_CHECK.set_failed(false);
+        }
+    }
+}
--- ld-tr/src/endpoint.rs
+++ ld-tr/src/endpoint.rs
@@ -1,9 +1,11 @@
 use actix_web::{HttpRequest, HttpResponse, Responder, post, web, get};
+use crate::balance_logic::call_payments;
 
 #[post("/payments")]
 pub async fn proxy_payment(req: HttpRequest, body: web::Bytes) -> impl Responder {
     println!("{:?}", req);
     println!("{:?}", body);
+    call_payments(req, body).await;
     HttpResponse::Ok()
 }
 
