c52ed64b92b5a8484150976fe552bbec3e75cda3
lopes <alexrozaestudos@gmail.com>
2025-07-12 11:38:18 -0300

    ok, ta quase 100

--- b-rs/src/main.rs
+++ b-rs/src/main.rs
@@ -15,7 +15,7 @@ async fn main() -> std::io::Result<()> {
             .service(get_payments_summary)
             .service(post_purge_payment)
     })
-    .bind(("0.0.0.0", 9999))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
