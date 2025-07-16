8a8398f59b5de2b89128ea3c5e272d117aff06f3
lopes <alexrozaestudos@gmail.com>
2025-07-16 20:31:48 -0300

    ajustes

--- ld-tr/src/payment.rs
+++ ld-tr/src/payment.rs
@@ -50,7 +50,7 @@ pub async fn payment(payment_req: PaymentRequest) {
     let full_url = format!("{}/payments", base_url);
 
     let result = timeout(
-        Duration::from_secs(6),
+        Duration::from_secs(3),
         client.post(&full_url).json(&response).send(),
     )
     .await;
@@ -68,7 +68,7 @@ pub async fn payment(payment_req: PaymentRequest) {
             let _ = enqueue_payment(response.clone()).await;
         }
         Err(_) => {
-            eprintln!("⏱ Timeout! A chamada demorou mais que 6 segundos.");
+            eprintln!("⏱ Timeout! A chamada demorou mais que 3 segundos.");
             let _ = enqueue_payment(response.clone()).await;
         }
     }
